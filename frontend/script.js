fe7a2701f6a660f4edb9f9f255b1379a4f32d2ed
Divyansh_Aggarwal <109619921+Divyansh1217@users.noreply.github.com>
2025-05-05 18:27:15 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://jina-ai-for-text-summerization-and.onrender.com/";
 
 function register() {
   const email = document.getElementById("regEmail").value;
